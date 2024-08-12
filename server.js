c046657871d1e5a5a6762a700bebfdd166b7dc36
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-12 16:52:56 +0200

    Simple routing

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ import http from "http";
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(500, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server error" }));
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage!</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About!</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not found!</h1>");
+      }
+    } else {
+      throw new Error("Method is not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/html" });
+    res.end(`<h1>Server error! ${error.message}</h1>`);
+  }
 });
 
 server.listen(PORT, () => {
